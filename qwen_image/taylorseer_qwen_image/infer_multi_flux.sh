97efb6474d1f115ba08cf9e586456dddb0a4ba1a
kalinplus <142799819+kalinplus@users.noreply.github.com>
2025-09-05 10:21:42 +0800

    feat: qwen-image batch infer comp

---
+++ qwen_image/taylorseer_qwen_image/infer_multi_flux.sh
@@ -0,0 +1,13 @@
+local_model_path="/data/public/model/Qwen/Qwen-Image"
+
+python qwen_image/taylorseer_qwen_image/batch_infer.py \
+    --model "$local_model_path" \
+    --steps 50 \
+    --height 1024 \
+    --width 1024 \
+    --seed 42 \
+    --dtype bfloat16 \
+    --true_cfg_scale 7.5 \
+    --outdir outputs \
+    --prefix TaylorSeer \
+    --prompt_file assets/prompts/miku.txt \
