b3a18f0b05f83eaebc3195d958e46a25624f86aa
kalinplus <kalinplus@outlook.com>
2025-09-09 19:19:35 +0800

    geneval

--- qwen_image/infer_single_qwen_image.sh
+++ qwen_image/infer_single_qwen_image.sh
@@ -1,7 +1,6 @@
 local_model_path="/data/public/model/Qwen/Qwen-Image"
 model_id="Qwen/Qwen-Image"
-# export CUDA_VISIBLE_DEVICES='5, 6'
-export ACCELERATE_DEVICE_MAP='{"text_encoder":0,"transformer":1,"vae":2,"final_layer":3,"image_proj_model":3}'
+export CUDA_VISIBLE_DEVICES='3, 4, 5, 6'
 
 # cd qwen_image
 python qwen_image/taylorseer_qwen_image/diffusers_taylorseer_qwen_image.py \
@@ -13,3 +12,4 @@ python qwen_image/taylorseer_qwen_image/diffusers_taylorseer_qwen_image.py \
     --outdir outputs \
     --prefix TaylorSeer \
     --prompt "A coffee shop entrance features a chalkboard sign reading 'Qwen Coffee 😊 $2 per cup,' with a neon light beside it displaying '通义千问'. Next to it hangs a poster showing a beautiful Chinese woman, and beneath the poster is written 'π≈3.1415926-53589793-23846264-33832795-02384197'." \
+    --use_taylor \
