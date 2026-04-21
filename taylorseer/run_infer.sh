640f8698e6866d6369c5e7a5a88c3711df6c617b
kalinplus <142799819+kalinplus@users.noreply.github.com>
2026-04-21 10:53:10 +0000

    chore: modify sh

--- taylorseer/run_infer.sh
+++ taylorseer/run_infer.sh
@@ -13,35 +13,37 @@ export SDXL_MODEL_PATH="$local_model_path"
 # Sampling parameters
 height=1024
 width=1024
-num_steps=50
 guidance=5.0
 seed=0
 
 # Cache parameters
-intervals=(3 5 6)
-max_order=(0 1 2)
-first_enhance=3
+num_steps_list=(50 16 10)
+intervals=(0)
+max_order=(0)
+first_enhance=50
 
 for interval in "${intervals[@]}"; do
     for order in "${max_order[@]}"; do
         echo "Running interval=$interval, order=$order ..."
 
-        outdir="${base_outdir}/N${interval}O${order}F${first_enhance}"
+        for num_steps in "${num_steps_list[@]}"; do
+            outdir="${base_outdir}/S${num_steps}/N${interval}O${order}F${first_enhance}"
 
-        torchrun --nproc_per_node=4 sample.py \
-            --prompt_file "$prompt_file" \
-            --height "$height" \
-            --width "$width" \
-            --num_steps "$num_steps" \
-            --guidance "$guidance" \
-            --seed "$seed" \
-            --output_dir "$outdir" \
-            --interval "$interval" \
-            --max_order "$order" \
-            --first_enhance "$first_enhance"
+            torchrun --nproc_per_node=4 sample.py \
+                --prompt_file "$prompt_file" \
+                --height "$height" \
+                --width "$width" \
+                --num_steps "$num_steps" \
+                --guidance "$guidance" \
+                --seed "$seed" \
+                --output_dir "$outdir" \
+                --interval "$interval" \
+                --max_order "$order" \
+                --first_enhance "$first_enhance"
 
-        echo "Finished interval=$interval, order=$order. Results saved to $outdir"
-        echo "-------------N${interval}O${order}F${first_enhance}-------------"
+            echo "Finished interval=$interval, order=$order, num_steps=$num_steps. Results saved to $outdir"
+            echo "-------------S${num_steps}N${interval}O${order}F${first_enhance}-------------"
+        done
     done
 done
 
